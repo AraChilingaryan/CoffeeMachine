c74dca2bc55929c33b6d78d730f57f0364b50f15
Ara Chilingaryan <chilingaryanara96@gmail.com>
2020-07-20 23:27:22 +0400

    - added new file to be ignored -- added new MachinesNeeds class impl

--- MachinesHaveNeeds/src/MachinesNeeds.java
+++ MachinesHaveNeeds/src/MachinesNeeds.java
@@ -1,2 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MachinesNeeds {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Write how many cups of coffee you will need:");
+        System.out.print(">");
+        int countOfCups = Integer.parseInt(reader.readLine());
+        System.out.println("For " + countOfCups +  " cups of coffee you will need:");
+        System.out.println(countOfCups * 200 + " ml of water");
+        System.out.println(countOfCups * 50 + " ml of milk");
+        System.out.println(countOfCups * 15 + " ml of coffee beans");
+    }
 }
