f6e36ed733329ff7385a6965b82d742c7f2be51a
Ara Chilingaryan <chilingaryanara96@gmail.com>
2020-07-22 10:44:36 +0400

    - changed all if-else to switch-case - big methods separated into little methods

--- Action/src/Action.java
+++ Action/src/Action.java
@@ -16,76 +16,85 @@ public class Action {
         System.out.println(countOfMilk + " of milk");
         System.out.println(countOfCoffeeBeans + " of coffee beans");
         System.out.println(countOfMoney + " of money");
-        System.out.println("Write action (buy, fill, take):");
-        System.out.print(">");
+        System.out.print("Write action (buy, fill, take):\n>");
+        //System.out.print(">");
         String action = reader.readLine();
+        switch (action){
+            case "buy":
+                buy(countOfWater,countOfMilk,countOfCoffeeBeans,countOfMoney,countOfCups);
+                break;
+            case "fill":
+                fill(countOfWater,countOfMilk,countOfCoffeeBeans,countOfMoney,countOfCups);
+                break;
+            case "take":
+                take(countOfWater,countOfMilk,countOfCoffeeBeans,countOfMoney,countOfCups);
+                break;
 
-        if(action.equals("buy"))
-            buy(countOfWater,countOfMilk,countOfCoffeeBeans,countOfMoney,countOfCups);
-        if(action.equals("fill"))
-            fill(countOfWater,countOfMilk,countOfCoffeeBeans,countOfMoney,countOfCups);
-        if(action.equals("take"))
-            take(countOfWater,countOfMilk,countOfCoffeeBeans,countOfMoney,countOfCups);
+        }
+
+
 
         reader.close();
 
     }
     public static void buy(int countOfWater, int countOfMilk,int countOfCoffeeBeans, int countOfMoney,int countOfCups) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        System.out.print(">");
+        System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n>");
         String number = reader.readLine();
-        System.out.println(number);
-        if(number.equals("1")){
-            System.out.println("The coffee machine has:");
-            System.out.println(countOfWater - 250 + " of water");
-            System.out.println(countOfMilk + " of milk");
-            System.out.println(countOfCoffeeBeans - 16 + " of coffee beans");
-            System.out.println(countOfCups - 1 + " of disposable cups");
-            System.out.println(countOfMoney + 4 + " of money");
-        }
-        if(number.equals("2")){
-            System.out.println("The coffee machine has:");
-            System.out.println(countOfWater - 350 + " of water");
-            System.out.println(countOfMilk - 75 + " of milk");
-            System.out.println(countOfCoffeeBeans - 20 + " of coffee beans");
-            System.out.println(countOfCups - 1 + " of disposable cups");
-            System.out.println(countOfMoney + 7 + " of money");
+        switch (Integer.parseInt(number)){
+            case 1:
+                espresso(countOfWater, countOfMilk,countOfCoffeeBeans,countOfCups,countOfMoney);
+                break;
+            case 2:
+                latte(countOfWater, countOfMilk,countOfCoffeeBeans,countOfCups,countOfMoney);
+                break;
+            case 3:
+                cappuccino(countOfWater, countOfMilk,countOfCoffeeBeans,countOfCups,countOfMoney);
+                break;
         }
 
-        if(number.equals("3")){
-            System.out.println("The coffee machine has:");
-            System.out.println(countOfWater - 200 + " of water");
-            System.out.println(countOfMilk - 100 + " of milk");
-            System.out.println(countOfCoffeeBeans - 12 + " of coffee beans");
-            System.out.println(countOfCups - 1 + " of disposable cups");
-            System.out.println(countOfMoney + 6 + " of money");
-        }
         reader.close();
 
     }
+    public static void espresso(int countOfWater, int countOfMilk,int countOfCoffeeBeans,int countOfCups, int countOfMoney){
+        System.out.println("The coffee machine has:");
+        System.out.println(countOfWater - 250 + " of water");
+        System.out.println(countOfMilk + " of milk");
+        System.out.println(countOfCoffeeBeans - 16 + " of coffee beans");
+        System.out.println(countOfCups - 1 + " of disposable cups");
+        System.out.println(countOfMoney + 4 + " of money");
+
+    }
+
+    public static void latte(int countOfWater, int countOfMilk,int countOfCoffeeBeans,int countOfCups, int countOfMoney){
+        System.out.println("The coffee machine has:");
+        System.out.println(countOfWater - 350 + " of water");
+        System.out.println(countOfMilk - 75 + " of milk");
+        System.out.println(countOfCoffeeBeans - 20 + " of coffee beans");
+        System.out.println(countOfCups - 1 + " of disposable cups");
+        System.out.println(countOfMoney + 7 + " of money");
+    }
+
+    public static void cappuccino(int countOfWater, int countOfMilk,int countOfCoffeeBeans,int countOfCups, int countOfMoney){
+        System.out.println("The coffee machine has:");
+        System.out.println(countOfWater - 200 + " of water");
+        System.out.println(countOfMilk - 100 + " of milk");
+        System.out.println(countOfCoffeeBeans - 12 + " of coffee beans");
+        System.out.println(countOfCups - 1 + " of disposable cups");
+        System.out.println(countOfMoney + 6 + " of money");
+    }
+
 
     public static void fill(int countOfWater, int countOfMilk,int countOfCoffeeBeans, int countOfMoney,int countOfCups) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Write how many ml of water do you want to add:");
-        System.out.print(">");
-        int countOfWater1 = Integer.parseInt(reader.readLine());
-        System.out.println(countOfWater);
-
-        System.out.println("Write how many ml of milk do you want to add:");
-        System.out.print(">");
-        int countOfMilk1 = Integer.parseInt(reader.readLine());
-        System.out.println(countOfMilk);
-
-        System.out.println("Write how many grams of coffee beans do you want to add:");
-        System.out.print(">");
-        int countOfCoffeeBeans1 = Integer.parseInt(reader.readLine());
-        System.out.println(countOfCoffeeBeans);
-
-        System.out.println("Write how many disposable cups of coffee do you want to add:");
-        System.out.print(">");
-        int countOfCups1 = Integer.parseInt(reader.readLine());
-        System.out.println(countOfCups);
+        System.out.print("Write how many ml of water do you want to add:\n>");
+           int countOfWater1 = Integer.parseInt(reader.readLine());
+        System.out.print("Write how many ml of milk do you want to add:\n>");
+           int countOfMilk1 = Integer.parseInt(reader.readLine());
+        System.out.print("Write how many grams of coffee beans do you want to add:\n>");
+           int countOfCoffeeBeans1 = Integer.parseInt(reader.readLine());
+        System.out.print("Write how many disposable cups of coffee do you want to add:\n>");
+           int countOfCups1 = Integer.parseInt(reader.readLine());
 
         System.out.println("The coffee machine has:");
         System.out.println(countOfWater + countOfWater1 + " of water");
