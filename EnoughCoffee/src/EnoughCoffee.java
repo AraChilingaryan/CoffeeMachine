7015534d67775d0f9ec42a9e709b6014a46205ae
Ara Chilingaryan <chilingaryanara96@gmail.com>
2020-07-22 12:18:50 +0400

    - splited on small methods - changed all ">"

--- EnoughCoffee/src/EnoughCoffee.java
+++ EnoughCoffee/src/EnoughCoffee.java
@@ -1,44 +1,51 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class EnoughCoffee {
     public static void main(String[] args) throws Exception {
+        int[] array = enoughCoffee();
+        isEnough(array);
+    }
+
+    public static int[] enoughCoffee() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<Integer> list = new ArrayList<>();
 
-        System.out.println("Write how many ml of water the coffee machine has:");
-        System.out.print(">");
-        String countOfWater = bufferedReader.readLine();
-        list.add(Integer.parseInt(countOfWater) / 200);
+        System.out.print("Write how many ml of water the coffee machine has:\n>");
+        list.add(Integer.parseInt(bufferedReader.readLine()) / 200);
 
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        System.out.print(">");
-        String countOfMilk = bufferedReader.readLine();
-        list.add(Integer.parseInt(countOfMilk) / 50);
+        System.out.print("Write how many ml of milk the coffee machine has:\n>");
+        list.add(Integer.parseInt(bufferedReader.readLine()) / 50);
 
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        System.out.print(">");
-        String countOfCoffee_Beans = bufferedReader.readLine();
-        list.add(Integer.parseInt(countOfCoffee_Beans) / 15);
+        System.out.print("Write how many grams of coffee beans the coffee machine has:\n>");
+        list.add(Integer.parseInt(bufferedReader.readLine()) / 15);
 
-        System.out.println("Write how many cups of coffee you will need:");
-        System.out.print(">");
+        System.out.print("Write how many cups of coffee you will need:\n>");
         String count = bufferedReader.readLine();
 
         Collections.sort(list);
-        if(list.get(0) < Integer.parseInt(count)){
-            System.out.println("No, I can make only " + list.get(0) + " cup(s) of coffee");
+        bufferedReader.close();
+        int[] arrayOfArgument = new int[2];
+        arrayOfArgument[0] = list.get(0);
+        arrayOfArgument[1] = Integer.parseInt(count);
+
+        return arrayOfArgument;
+    }
+
+    public static void isEnough(int[] array){
+        if(array[0] < array[1]){
+            System.out.println("No, I can make only " + array[0] + " cup(s) of coffee");
         }
 
-        if(list.get(0) == Integer.parseInt(count)){
+        if(array[0] == array[1]){
             System.out.println("Yes, I can make that amount of coffee");
         }
 
-        if(list.get(0) > Integer.parseInt(count)){
-            System.out.println("Yes, I can make that amount of coffee" + " (and even " + (list.get(0) - Integer.parseInt(count)) + " more than that)");
+        if(array[0] > array[1]){
+            System.out.println("Yes, I can make that amount of coffee" + " (and even " + (array[0] - array[1]) + " more than that)");
         }
-
     }
 }
