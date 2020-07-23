7993d2205fea2d3d060fcafb0bcdbdc20fea3215
Ara Chilingaryan <chilingaryanara96@gmail.com>
2020-07-23 18:50:46 +0400

    - added comment in CoffeeLoop - changed names of arrays to meaningfull names

--- Coffee_loop/src/CoffeeLoop.java
+++ Coffee_loop/src/CoffeeLoop.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class CoffeeLoop {
@@ -11,12 +8,12 @@ public class CoffeeLoop {
         int countOfCups = 9;
         int countOfMoney = 550;
 
-        int[] array = new int[5];
-        array[0] = countOfWater;
-        array[1] = countOfMilk;
-        array[2] = countOfCoffeeBeans;
-        array[3] = countOfCups;
-        array[4] = countOfMoney;
+        int[] arrayOfIngredients = new int[5];
+        arrayOfIngredients[0] = countOfWater;
+        arrayOfIngredients[1] = countOfMilk;
+        arrayOfIngredients[2] = countOfCoffeeBeans;
+        arrayOfIngredients[3] = countOfCups;
+        arrayOfIngredients[4] = countOfMoney;
 
        Scanner scanner = new Scanner((System.in));
         boolean isTrue = true;
@@ -28,16 +25,16 @@ public class CoffeeLoop {
 
             switch (action) {
                 case "buy":
-                    array = buy(array);
+                    arrayOfIngredients = buy(arrayOfIngredients);
                     break;
                 case "fill":
-                    array = fill(array);
+                    arrayOfIngredients = fill(arrayOfIngredients);
                     break;
                 case "take":
-                    array = take(array);
+                    arrayOfIngredients = take(arrayOfIngredients);
                     break;
                 case "remain":
-                    array = remain(array);
+                    arrayOfIngredients = remain(arrayOfIngredients);
                     break;
                 case "exit":
                     exit();
@@ -47,157 +44,197 @@ public class CoffeeLoop {
         }
 
     }
-    public static int[]  buy(int[] array1) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    /**
+     * prints what kind of coffee hs machine
+     * @return array of ingredients
+     */
+    public static int[]  buy(int[] arrayOfIngredients) {
+
+        Scanner scanner = new Scanner(System.in);
         System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:\n>");
-        String number = null;
-        try {
-            number = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        String number = scanner.nextLine();
+
         switch(Integer.parseInt(number)){
             case 1:
-                espresso(array1);
+                espresso(arrayOfIngredients);
                 break;
             case 2:
-                latte(array1);
+                latte(arrayOfIngredients);
                 break;
             case 3:
-            cappuccino(array1);
+            cappuccino(arrayOfIngredients);
             break;
 
         }
-        return array1;
+        return arrayOfIngredients;
     }
 
-    public static void espresso(int[] array1){
+    /**
+     * prints does machine have enough resources for Espresso
+     * and changes argument's parameters
+     * @param arrayOfIngredients
+     */
+
+    public static void espresso(int[] arrayOfIngredients){
         int countOfWaterForEspresso = 250;
         int countOfCoffeeBeansForEspresso = 16;
         int countOfCupsForEspresso = 1;
         int countOfMoneyForEspresso = 4;
-        if(array1[0] < countOfWaterForEspresso)
+        if(arrayOfIngredients[0] < countOfWaterForEspresso)
         {
             System.out.println("Sorry, not enough water!");
         }
-        else if(array1[2] < countOfCoffeeBeansForEspresso)
+        else if(arrayOfIngredients[2] < countOfCoffeeBeansForEspresso)
         {
             System.out.println("Sorry, not enough coffee beans!");
         }
         else
         {
             System.out.println("I have enough resources, making you a coffee!");
-            array1[0] -= countOfWaterForEspresso;
-            array1[2] -= countOfCoffeeBeansForEspresso;
-            array1[3] -= countOfCupsForEspresso;
-            array1[4] += countOfMoneyForEspresso;
+            arrayOfIngredients[0] -= countOfWaterForEspresso;
+            arrayOfIngredients[2] -= countOfCoffeeBeansForEspresso;
+            arrayOfIngredients[3] -= countOfCupsForEspresso;
+            arrayOfIngredients[4] += countOfMoneyForEspresso;
         }
 
     }
+    /**
+     * prints does machine have enough resources for latte
+     * and changes argument's parameters
+     * @param arrayOfIngredients
+     */
 
-    public static void latte(int[] array1){
+    public static void latte(int[] arrayOfIngredients){
         int countOfWaterForLatte = 350;
         int countOfMilkForLatte = 75;
         int countOfCoffeeBeansForLatte = 20;
         int countOfCupsForLatte = 1;
         int countOfMoneyForLatte = 7;
-        if(array1[0] < countOfWaterForLatte)
+        if(arrayOfIngredients[0] < countOfWaterForLatte)
         {
             System.out.println("Sorry, not enough water!");
         }
-        else if(array1[1] < countOfMilkForLatte)
+        else if(arrayOfIngredients[1] < countOfMilkForLatte)
         {
             System.out.println("Sorry, not enough milk!");
         }
-        else if(array1[2] < countOfCoffeeBeansForLatte)
+        else if(arrayOfIngredients[2] < countOfCoffeeBeansForLatte)
         {
             System.out.println("Sorry, not enough coffee beans!");
         }
         else
         {
             System.out.println("I have enough resources, making you a coffee!");
-            array1[0] -= countOfWaterForLatte;
-            array1[1] -= countOfMilkForLatte;
-            array1[2] -= countOfCoffeeBeansForLatte;
-            array1[3] -= countOfCupsForLatte;
-            array1[4] += countOfMoneyForLatte;
+            arrayOfIngredients[0] -= countOfWaterForLatte;
+            arrayOfIngredients[1] -= countOfMilkForLatte;
+            arrayOfIngredients[2] -= countOfCoffeeBeansForLatte;
+            arrayOfIngredients[3] -= countOfCupsForLatte;
+            arrayOfIngredients[4] += countOfMoneyForLatte;
         }
 
     }
-    public static void cappuccino(int[] array1){
+    /**
+     * prints does machine have enough resources for cappuccino
+     * and changes argument's parameters
+     * @param arrayOfIngredients
+     */
+    public static void cappuccino(int[] arrayOfIngredients){
         int countOfWaterForCappuccino = 200;
         int countOfMilkForCappuccino = 100;
         int countOfCoffeeBeansForCappuccino = 12;
         int countOfCupsForCappuccino = 1;
         int countOfMoneyForCappuccino = 6;
-        if(array1[0] < countOfWaterForCappuccino)
+        if(arrayOfIngredients[0] < countOfWaterForCappuccino)
         {
             System.out.println("Sorry, not enough water!");
         }
-        else if(array1[1] < countOfMilkForCappuccino)
+        else if(arrayOfIngredients[1] < countOfMilkForCappuccino)
         {
             System.out.println("Sorry, not enough milk!");
         }
-        else if(array1[2] < countOfCoffeeBeansForCappuccino)
+        else if(arrayOfIngredients[2] < countOfCoffeeBeansForCappuccino)
         {
             System.out.println("Sorry, not enough coffee beans!");
         }
         else
         {
             System.out.println("I have enough resources, making you a coffee!");
-            array1[0] -= countOfWaterForCappuccino;
-            array1[1] -= countOfMilkForCappuccino;
-            array1[2] -= countOfCoffeeBeansForCappuccino;
-            array1[3] -= countOfCupsForCappuccino;
-            array1[4] += countOfMoneyForCappuccino;
+            arrayOfIngredients[0] -= countOfWaterForCappuccino;
+            arrayOfIngredients[1] -= countOfMilkForCappuccino;
+            arrayOfIngredients[2] -= countOfCoffeeBeansForCappuccino;
+            arrayOfIngredients[3] -= countOfCupsForCappuccino;
+            arrayOfIngredients[4] += countOfMoneyForCappuccino;
         }
 
     }
 
-    public static int[]  fill(int[] array1) {
+    /**
+     * prints what and how much we want to add
+     * @param   arrayOfIngredients
+     * @return changed arrayOfArguments
+     */
+
+    public static int[]  fill(int[] arrayOfIngredients) {
         Scanner scanner = new Scanner((System.in));
 
         System.out.print("Write how many ml of water do you want to add:\n>");
         int countOfWater = scanner.nextInt();
-        array1[0] += countOfWater;
+        arrayOfIngredients[0] += countOfWater;
 
         System.out.print("Write how many ml of milk do you want to add:\n>");
         int countOfMilk = scanner.nextInt();
-        array1[1] +=  countOfMilk;
+        arrayOfIngredients[1] +=  countOfMilk;
 
         System.out.print("Write how many grams of coffee beans do you want to add:\n>");
 
         int countOfCoffeeBeans = scanner.nextInt();
-        array1[2] += countOfCoffeeBeans;
+        arrayOfIngredients[2] += countOfCoffeeBeans;
 
         System.out.print("Write how many disposable cups of coffee do you want to add:\n>");
         int countOfCups = scanner.nextInt();
-        array1[3] += countOfCups;
+        arrayOfIngredients[3] += countOfCups;
 
 
-        return array1;
+        return arrayOfIngredients;
     }
 
-    public static int[]  take(int[] array1) {
-        int amountOfMoney = array1[4];
+    /**
+     * prints and give us money from machine
+     * @param arrayOfIngredients
+     * @return changed arrayOfIngredients
+     */
+
+    public static int[]  take(int[] arrayOfIngredients) {
+        int amountOfMoney = arrayOfIngredients[4];
         if(amountOfMoney != 0) {
             System.out.println("I gave you " + amountOfMoney);
-            array1[4] = 0;
+            arrayOfIngredients[4] = 0;
         }else System.out.println("We don't have money for you:(");
-        return array1;
+        return arrayOfIngredients;
     }
 
-    public static int[]  remain(int[] array1) {
+    /**
+     * prints how much ingredients has machune at that moment
+     * @param arrayOfIngredients
+     * @return  arrayOfIngredients
+     */
+    public static int[]  remain(int[] arrayOfIngredients) {
         System.out.println("The coffee machine has:");
-        System.out.println(array1[0] + " of water");
-        System.out.println(array1[1] + " of milk");
-        System.out.println(array1[2] + " of coffee beans");
-        System.out.println(array1[3] + " of disposable cups");
-        System.out.println(array1[4] + " of money");
+        System.out.println(arrayOfIngredients[0] + " of water");
+        System.out.println(arrayOfIngredients[1] + " of milk");
+        System.out.println(arrayOfIngredients[2] + " of coffee beans");
+        System.out.println(arrayOfIngredients[3] + " of disposable cups");
+        System.out.println(arrayOfIngredients[4] + " of money");
 
-        return array1;
+        return arrayOfIngredients;
 
     }
+
+    /**
+     * calling exit machine interrupts its work
+     * and program finished with 0;
+     */
     public static void exit(){
         return;
     }
